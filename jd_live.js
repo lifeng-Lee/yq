a8dd3655154473ae178c198a4617e5aec416726d
lifeng <73932388+lifeng-Lee@users.noreply.github.com>
2022-06-24 14:38:22 +0800

    Update jd_live.js

--- jd_live.js
+++ jd_live.js
@@ -32,10 +32,10 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  if (!jdPandaToken) {
-    console.log('请填写Panda获取的Token,变量是PandaToken');
-    return;
-  }
+//   if (!jdPandaToken) {
+//     console.log('请填写Panda获取的Token,变量是PandaToken');
+//     return;
+//   }
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
